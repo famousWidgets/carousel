993def1ae510925d8556a9ffe94448e1ceef4f14
Willson Mock <willson.mock@gmail.com>
2014-04-25 19:08:22 -0700

    Fill in ScrollItemView class

--- app/src/views/ScrollItemView.js
+++ app/src/views/ScrollItemView.js
@@ -11,14 +11,57 @@ define(function(require, exports, module) {
      * @description
      */
 
-    function ScrollItemView() {
+    var ScrollItemView = function () {
         View.apply(this, arguments);
-    }
+        
+        // modifiers must be called first
+        addSizeModifier.call(this);
+        addStateModifier.call(this);
+
+        addSurface.call(this);
+    };
 
     ScrollItemView.prototype = Object.create(View.prototype);
     ScrollItemView.prototype.constructor = ScrollItemView;
 
     ScrollItemView.DEFAULT_OPTIONS = {
+        xScale: 2,
+        yScale: 2,
+        scaleDuration: 1000
+    };
+
+    var addSurface = function (color, size) {
+        this.surface = new Surface({
+            size : [size, size],
+            properties: {
+                backgroundColor: color
+            }
+        });
+
+        addListener.call(this.surface);
+    };
+
+    var addSizeModifier = function () {
+        this.sizeModifier = new StateModifier({
+            size: [size, size]
+        });
+    };
+
+    var addStateModifier = function () {
+        this.stateModifier = new StateModifier();
+    };
+
+    var addListener = function () {
+        // 'this' will be surface
+        this.stateModifier.setTransform(
+            Transform.scale(xScale, yScale, 1),
+            { duration: scaleDuration } 
+        );
+
+        this.sizeModifier.setSize(
+            [this.getSize()[0].xScale, this.getSize()[1].yScale],
+            { duration: scaleDuration }
+        );
     };
 
     module.exports = ScrollItemView;
