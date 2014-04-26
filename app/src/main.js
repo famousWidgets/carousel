0425a2ed6ce57bdb57870889a09ce782e94a28d9
Willson Mock <willson.mock@gmail.com>
2014-04-25 21:08:10 -0700

    Clean up main.js

--- app/src/main.js
+++ app/src/main.js
@@ -14,96 +14,47 @@ define(function(require, exports, module) {
     var RenderNode = require('famous/core/RenderNode');
     var Transform = require('famous/core/Transform');
     var View = require('famous/core/View');
+    var Draggable = require('famous/modifiers/Draggable');
 
     var ScrollItemView = require('./views/ScrollItemView');
 
-    /* SETUP */
-
-    // contains all surfaces - to be used with scrollView
-    var surfaces = [];
-
-    // create surface
-    var createSurface = function (color, size) {
-        var surface = new Surface({
-            size : [size, size],
-            properties: {
-                backgroundColor: color
-            }
-        });
-
-        // adding modifiers
-        var surfaceModifier = new StateModifier();
-        var sizeModifier = new StateModifier({
-            size: [size, size]
-        });
-
-        // creating a render node as an intermediary
-        var renderNode = new RenderNode();
-        renderNode.add(sizeModifier).add(surfaceModifier).add(surface);
-
-        // var view = new View();
-
-        // exposing modifiers on the surface
-        surface.modifier = surfaceModifier;
-        surface.sizeModifier = sizeModifier;
-        
-        // adding events
-        addEvent('click', surface);
-
-        // return view.add(surfaceModifier).add(surface);
-        // console.log('rn size: ', renderNode.getSize());
-        return renderNode;
-    };
-
-    // create an array of surfaces 
-    var createSurfaceArray = function (num, size) {
-        for (var i = 0; i < num; i++) {
+    var createScrollItemArray = function (num, size) {
+        for (var i = 0; i < num; i += 1) {
             var color = "hsl(" + (i * 360 / 10) + ", 100%, 50%)";
-            var surface = createSurface(color, size);
+            var scrollItemView = new ScrollItemView(color, size);
 
             // surfaces is closure-scoped
-            surfaces.push(surface);
+            scrollItemViews.push(scrollItemView);
+            scrollView.subscribe(scrollItemView._eventOutput);
         }
     };
 
-    var addEvent = function (listener, surface) {
-        surface.on(listener, function (e) {
-            // console.log('scroll init pos: ', scrollView.getPosition());
-
-            // scrollView is closure-scoped
-            scrollView.goToNextPage();
-
-            surface.modifier.setTransform(
-                Transform.scale(2,2,1),
-                {duration: 1000} 
-            );
-
-            surface.sizeModifier.setSize([200, 200], {duration: 1000});
-            console.log('scroll getPosition: ', scrollView.getPosition());
-        });
-    };
-
-    var modifier = new StateModifier();
-
     /* MAIN */
+
+    // contains list of scrollItemViews - to be used with scrollView
+    var scrollItemViews = [];
 
     // create main context
     var mainContext = Engine.createContext();
 
-    // create surface and append to surfaces array
-    createSurfaceArray(4, 100);
-
-    // create scrollview
     var scrollView = new Scrollview({
         direction: Utility.Direction.X
+        // paginated: true
     });
 
     var scrollViewModifier = new StateModifier({
         origin: [0, 0.5]
     });
 
-    scrollView.sequenceFrom(surfaces);
-    
+    // var draggable = new Draggable({
+    //     xRange: [-220, 220],
+    //     yRange: [0, 0]
+    // });
+
+    // populate scrollItemViews array
+    createScrollItemArray(4, 100);
+    scrollView.sequenceFrom(scrollItemViews);
+
     mainContext.setPerspective(500);
     mainContext.add(scrollViewModifier).add(scrollView);
 });
