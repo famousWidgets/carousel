a7229964d7f878a1eab0bff5f7d160e6a10bdc1e
Willson Mock <willson.mock@gmail.com>
2014-04-28 09:52:14 -0700

    Remove 'old code' section

--- app/src/views/ScrollItemView.js
+++ app/src/views/ScrollItemView.js
@@ -81,35 +81,6 @@ define(function(require, exports, module) {
             }
         });
 
-        // OLD CODE
-        // this.surface.on(this.options.listener, function () {
-        //     // check if surface is small
-        //     if (!this.isLarge) {
-        //         this.stateModifier.setTransform(
-        //             Transform.scale(this.options.xScaleUp, this.options.yScaleUp, 1),
-        //             { duration: this.options.scaleDuration } 
-        //         );
-
-        //         this.sizeModifier.setSize(
-        //             [this.surface.getSize()[0] * this.options.xScaleUp, this.surface.getSize()[1] * this.options.yScaleUp],
-        //             { duration: this.options.scaleDuration }
-        //         );
-        //     } else {
-        //         // surface is large
-        //         this.stateModifier.setTransform(
-        //             Transform.scale(this.options.xScaleDown, this.options.yScaleDown, 1),
-        //             { duration: this.options.scaleDuration } 
-        //         );
-
-        //         this.sizeModifier.setSize(
-        //             [this.surface.getSize()[0], this.surface.getSize()[1]],
-        //             { duration: this.options.scaleDuration }
-        //         );
-        //     }
-        //     // toggle
-        //     this.isLarge = !this.isLarge;
-        // }.bind(this));
-
         this.surface.pipe(this._eventOutput);
 
         // add everything together
